4cc1d1bc6f773e87b2e2b4dbeffe2be606cfa0b5
qjv <tuliomumic@gmail.com>
2025-04-29 09:21:13 -0300

    updated ports

--- app.py
+++ app.py
@@ -31,4 +31,5 @@ def download(filename):
     return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
